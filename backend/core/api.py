ab338de1dc3276938f4b8e245cba2d42eacc27fd
kalexweb <127949788+kalexweb@users.noreply.github.com>
2024-08-14 12:07:45 +0500

    api

--- backend/core/api.py
+++ backend/core/api.py
@@ -16,6 +16,40 @@ class ORJSONRenderer(BaseRenderer):
 api = NinjaAPI(renderer=ORJSONRenderer())
 # api = NinjaAPI(docs_url=None)
 
+
+class DepartmentSchema(ModelSchema):
+    class Meta:
+        model = models.Department
+        fields = "__all__"
+
+@api.get("/departments", response=List[DepartmentSchema])
+def departments(request):
+    qs = models.Department.objects.all()
+    return qs
+
+
+class DirectionSchema(ModelSchema):
+    class Meta:
+        model = models.Direction
+        fields = "__all__"
+
+@api.get("/directions", response=List[DirectionSchema])
+def directions(request):
+    qs = models.Direction.objects.all()
+    return qs
+
+
+class LanguageSchema(ModelSchema):
+    class Meta:
+        model = models.Language
+        fields = "__all__"
+
+@api.get("/languages", response=List[LanguageSchema])
+def languages(request):
+    qs = models.Language.objects.all()
+    return qs
+
+
 class PlatformSchema(ModelSchema):
     class Meta:
         model = models.Platform
@@ -27,6 +61,16 @@ def platforms(request):
     return qs
 
 
+class ResourceCompetenceSchema(Schema):
+    resource_id: int = Field(..., alias="resource.pk")
+    competence: str
+
+@api.get("/resource_competences", response=List[ResourceCompetenceSchema])
+def resource_competences(request):
+    qs = models.ResourceCompetence.objects.all()
+    return qs
+
+
 class ResourceSchema(ModelSchema):
     class Meta:
         model = models.Resource
@@ -35,6 +79,39 @@ class ResourceSchema(ModelSchema):
 @api.get("/resources", response=List[ResourceSchema])
 def resources(request):
     qs = models.Resource.objects.all()
+    return qs
+
+
+class ResourceStampApplicationSchema(ModelSchema):
+    class Meta:
+        model = models.ResourceStampApplication
+        fields = "__all__"
+
+@api.get("/resource_stamp_applications", response=List[ResourceStampApplicationSchema])
+def resource_stamp_applications(request):
+    qs = models.ResourceStampApplication.objects.all()
+    return qs
+
+
+class RightholderSchema(ModelSchema):
+    class Meta:
+        model = models.Rightholder
+        fields = "__all__"
+
+@api.get("/rightholders", response=List[RightholderSchema])
+def rightholders(request):
+    qs = models.Rightholder.objects.all()
+    return qs
+
+
+class SubjectSchema(ModelSchema):
+    class Meta:
+        model = models.Subject
+        fields = "__all__"
+
+@api.get("/subjects", response=List[SubjectSchema])
+def subjects(request):
+    qs = models.Subject.objects.all()
     return qs
 
 
